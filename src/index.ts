530fd5203eb109c284c75b289252da300a5a7bc7
Carlos Henrique <ch.henriquevinha@gmail.com>
2025-11-25 21:43:24 -0300

    Add events listener connection

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
 console.log("Hello from mongo-bullet!");
+
+export const analiseQuery = (connection: any) => {
+  connection.on('commandStarted', (event: any) => {
+    console.log('[MongoDB] START:', {
+      command: event.commandName,
+      collection: event.command?.find || event.command?.aggregate,
+      filter: event.command?.filter,
+      pipeline: event.command?.pipeline,
+    });
+  });
+
+  connection.on('commandFailed', (event: any) => {
+    console.log('[MongoDB] FAIL:', {
+      command: event.commandName,
+      failure: event.failure,
+    });
+  });
+}
